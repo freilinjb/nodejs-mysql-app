0499a6b03b468cce4e731987b7f085f9758556f6
Freilinjb <freilinjb@gmail.com>
2019-12-26 01:07:55 -0400

    Express funcionando - Arrancamos desde qui

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-const express
+const express = require('express');
+const morgan = require('morgan');
+
+//Initializations
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 4000);
+
+//Middlewares
+app.use(morgan('dev'));
+
+//Global Variables
+
+
+//Routes
+
+//Public
+
+//Starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+})
