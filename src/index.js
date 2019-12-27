a238426df4ec41ee219e210a94c469f5598176cd
Freilinjb <freilinjb@gmail.com>
2019-12-26 22:00:48 -0400

    Empezando de nuevo - Otra vez

--- src/index.js
+++ src/index.js
@@ -10,36 +10,35 @@ const app = express();
 //Settings
 app.set('port', process.env.PORT || 4000);
 
-app.set('views',path.join(__dirname,'views'))
-app.engine('.hbs', exphbs({
-    defaultLayout: 'main',
-    //Agregando la carpeta layouts a para que nodejs la reconosca
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    //Agregando la carpeta partialas para que njs la reconosca
-    partialsDir: path.join(app.get('views', 'partials')),
-    //extension del motor de plantilla
-    extname: '.hbs',
-    //Confirugacion del motor de plantilla
-    helpers: require('./lib/handlebars')
-}));
+// app.set('views',path.join(__dirname,'views'))
+// app.engine('.hbs', exphbs({
+//     defaultLayout: 'main',
+//     //Agregando la carpeta layouts a para que nodejs la reconosca
+//     layoutsDir: path.join(app.get('views'), 'layouts'),
+//     //Agregando la carpeta partialas para que njs la reconosca
+//     partialsDir: path.join(app.get('views', 'partials')),
+//     //extension del motor de plantilla
+//     extname: '.hbs',
+//     //Confirugacion del motor de plantilla
+//     helpers: require('./lib/handlebars')
+// }));
 
 //Motor de plantilla
-app.set('view engine','.hbs');
-//Middlewares
-app.use(morgan('dev'));
-//Aceptar los datos que envia el usuario
-app.use(express.urlencoded({extends: false}));
-//Para recibir JSON desde nuestra aplicacion
-app.use(express.json());
+// app.set('view engine','.hbs');
+// //Middlewares
+// app.use(morgan('dev'));
+// //Aceptar los datos que envia el usuario
+// app.use(express.urlencoded({extends: false}));
+// //Para recibir JSON desde nuestra aplicacion
+// app.use(express.json());
 
 //Global Variables
 //Toma la informacion del usuario, toma lo que el servidor quiere reponder, toma una funcion para continuar el resto del codigo
 app.use((req, res, next) =>{
     next();
 })
-sdaf
 
-//Routesasdfsdfasdf
+//Routes
 app.use(require('./routes/index'));
 //Public
 app.use(express.static(path.join(__dirname, 'public')));
