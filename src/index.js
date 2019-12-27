95605c0f86e5fcf810f97ab7ccb27f85bd847168
Freilinjb <freilinjb@gmail.com>
2019-12-26 21:27:43 -0400

    Configurando el motor de plantilla handlebars

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const exphbs = require('express-handlebars');
+const path  = require('path');
 
 //Initializations
 const app = express();
 
 //Settings
 app.set('port', process.env.PORT || 4000);
+app.set('views',path.join(__dirname,'views'))
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views', 'partials')),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
 
+app.set('view engine','.hbs');
 //Middlewares
 app.use(morgan('dev'));
+//Aceptar los datos que envia el usuario
+app.use(express.urlencoded({extends: false}));
+//Para recibir JSON desde nuestra aplicacion
+app.use(express.json());
 
 //Global Variables
 
