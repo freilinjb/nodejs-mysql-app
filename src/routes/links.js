dbb0d83eeb138a31eda211b6a92b0bc11c84be56
Freilinjb <freilinjb@gmail.com>
2019-12-26 23:00:52 -0400

    Prueba de handlebar (MAGIA)

--- src/routes/links.js
+++ src/routes/links.js
@@ -4,8 +4,14 @@ const router = express.Router();
 //Coneccion de base de dato
 const pool = require('../database');
 
+//PROBANDO SI FUNCIONA EL DIRECTORIO
+// router.get('/add', (req, res)=>{
+//     res.send('form');
+// })
+
+
 router.get('/add', (req, res)=>{
-    res.send('form');
+    res.render('links/add');
 })
 
 module.exports = router;
