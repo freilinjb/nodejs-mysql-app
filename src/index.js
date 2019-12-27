6ce5cf36e004f67be1533d26ad39fca25b3f7fa9
Freilinjb <freilinjb@gmail.com>
2019-12-26 22:54:34 -0400

    Corrigiendo error Link - form

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ app.use((req, res, next) =>{
 //Routes
 app.use(require('./routes/index'));
 app.use(require('./routes/authentication'));
-app.use('links',require('./routes/links'));
+app.use('/links',require('./routes/links'));
 //Public
 app.use(express.static(path.join(__dirname, 'public')));
 
--- src/routes/links.js
+++ src/routes/links.js
@@ -4,6 +4,8 @@ const router = express.Router();
 //Coneccion de base de dato
 const pool = require('../database');
 
-
+router.get('/add', (req, res)=>{
+    res.send('form');
+})
 
 module.exports = router;
