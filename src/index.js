4c8244d7041f5136c2e052e1af8b4c59027a0953
Freilinjb <freilinjb@gmail.com>
2019-12-28 20:04:42 -0400

    Arreglando error Definitivo de Timeago

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.engine('.hbs', exphbs({
     layoutsDir:  path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs',
-    helpers: require('./lib/handlebars').default
+    helpers: require('./lib/handlebars')
     
 }))
 
