67cebd695ef4fd1ff603e7ed8df1d115d730ed41
Freilinjb <freilinjb@gmail.com>
2019-12-28 22:39:44 -0400

    agregando vista signup y ruas

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/signup', (req, res) =>{
+    res.render('auth/signup')
+})
+
+router.post('/signup', (req, res) =>{
+})
 
 module.exports = router;
